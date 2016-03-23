46452e35f3efe62bc0590c9f34b6b55a8ca5945f
Sven Nahnsen <qeana10@qbicheadnode.bwgrid.uni-tuebingen.de>
2016-03-23 15:37:02 +0100

    Update to running version

--- runWorkflow.py
+++ runWorkflow.py
@@ -2,6 +2,7 @@ from CTDopts.CTDopts import _InFile, CTDModel, args_from_file
 import sys
 import os
 import subprocess
+import shlex
 
 wf_dir = sys.argv[1]
 ctd_params = args_from_file(wf_dir + '/WORKFLOW-CTD')
@@ -9,28 +10,46 @@ ctd_files = args_from_file(wf_dir + '/IN-FILESTOSTAGE')
 
 command = 'configureStrelkaWorkflow.pl'
 
-data_path = os.path.join(wfdir, 'data')
-result_path = os.path.join(wfdir, 'result')
-db_path = os.path.join(wfdir, 'ref')
-log_path = os.path.join(wfdir 'log')
+data_path = os.path.join(wf_dir, 'data')
+result_path = os.path.join(wf_dir, 'result')
+db_path = os.path.join(wf_dir, 'ref')
+log_path = os.path.join(wf_dir, 'logs')
 
 path_to_config = '/lustre_cfc/software/qbic/strelka/1.0.14/etc/strelka_config_%s_default.ini'
 
-command += ' --tumor %s' % os.path.join(data_path, ctd_files['tumor'].split('/')[-1])
-command += ' --normal %s' % os.path.join((data_path, ctd_files['normal'].split('/')[-1])
-command += ' --ref %s' % os.path.join((db_path, ctd_files['db'][0].split('/')[-1])
+tumorFile = os.path.join(data_path, ctd_files['Tumor'].split('/')[-1])
+normalFile = os.path.join(data_path, ctd_files['Normal'].split('/')[-1])
+reference = os.path.join(db_path, ctd_files['db'].split('/')[-1])
+
+command += ' --tumor %s' % tumorFile
+command += ' --normal %s' % normalFile
+command += ' --ref %s' % reference
 command += ' --config %s' % path_to_config % ctd_params['read_mapper']
 command += ' --output-dir %s' % os.path.join(wf_dir, 'analysis')
 
+commandSam = 'samtools index {f}'
+commandSam2 = 'samtools faidx {f}'
+
+logfilename = 'somaticvariantcalling_1_0_workflow.logs'
+logfile = open(logfilename, 'w')
+
+subprocess.call(shlex.split(commandSam.format(f=normalFile)), stdout=logfile, stderr=logfile)
+subprocess.call(shlex.split(commandSam.format(f=tumorFile)), stdout=logfile, stderr=logfile)
+subprocess.call(shlex.split(commandSam2.format(f=reference)), stdout=logfile, stderr=logfile)
+
 output_folder = os.path.join(wf_dir, 'analysis')
-subprocess.call(command.split())
-os.chdir(output_folder)
+subprocess.call(shlex.split(command), stderr=logfile, stdout=logfile)
+#os.chdir(output_folder)
+analysisCommand = 'make -j 16 -C {f}'
+subprocess.call(shlex.split(analysisCommand.format(f=output_folder)), stdout=logfile, stderr=logfile)
 
-with open(os.path.join(log_path,'log.txt'),"wb") as out:
-        subprocess.call(['make', '-j', '16'], stdout=out)
+#os.chdir(os.path.join(output_folder, 'results'))
+vcfCommand = 'vcfoverlay {i} {s}'
 
+with open(os.path.join(output_folder, 'results/somatic.variants.vcf'), 'w') as f:
+    subprocess.call(shlex.split(vcfCommand.format(i=os.path.join(output_folder, 'results/all.somatic.snvs.vcf'), s=os.path.join(output_folder, 'results/all.somatic.indels.vcf'))), stdout=f,stderr=logfile)
 
-os.chdir(os.path.join(output_folder, 'results'))
-subprocess.call(['/lustre_cfc/software/qbic/vcflib/bin/vcfoverlay', 'all.somatic.indels.vcf',  'all.somatic.snvs.vcf', '>', 'somatic.variants.vcf'])
+subprocess.call(['cp', os.path.join(output_folder, 'results/somatic.variants.vcf'), result_path], stderr=logfile, stdout=logfile)
 
-subprocess.call(['cp', 'somatic.variants.vcf', result_path])
+logfile.close()
+subprocess.call(["mv", logfilename, log_path])
--- workflow.sh
+++ workflow.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-module load qbic/anaconda2
+module load qbic/anaconda2/2.1.0
 module load qbic/strelka/1.0.14
+module load qbic/vcflib/0.1
+module load qbic/samtools/1.3
 
 workflowDir=$(cat wfdir)
 #parse using CTDopts and run workflow
